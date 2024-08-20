d1556adee44e37fd45fe4558703fa070af0e040d
Bullskc <bullskc@naver.com>
2024-08-21 07:12:25 +0900

    feat: Optimize image loading with lazy loading

--- School/frontPage.py
+++ School/frontPage.py
@@ -9,6 +9,23 @@ from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 from reportlab.lib import colors
 
+# import os
+# import subprocess
+
+# def system_call(args, cwd="."):
+#     print("Running '{}' in '{}'".format(str(args), cwd))
+#     subprocess.call(args, cwd=cwd)
+#     pass
+
+# def fix_image_files(root=os.curdir):
+#     for path, dirs, files in os.walk(os.path.abspath(root)):
+#         # sys.stdout.write('.')
+#         for dir in dirs:
+#             system_call("mogrify *.png", "{}".format(os.path.join(path, dir)))
+
+
+# fix_image_files(os.curdir)
+
 
 class MySideBar(QMainWindow, Ui_MainWindow):
     def __init__(self):
